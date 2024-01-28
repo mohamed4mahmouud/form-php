18725095dcf4b85dc364fe629f9dbbb14e933f6f
mohamed4mahmouud <mohamedalamrawy55@gmail.com>
2024-01-28 17:09:28 +0200

    edit

--- index.php
+++ index.php
@@ -7,22 +7,20 @@ include "./form.html"
 //print_r($_REQUEST['Courses']);
 
 if(isset($_REQUEST['submit-btn'])){
-   if($_REQUEST['name'] || $_REQUEST['e-mail'] || $_REQUEST['group'] || $_REQUEST['class'] || isset($_REQUEST['gender'])){
+   if($_REQUEST['name'] || $_REQUEST['e-mail'] || $_REQUEST['group'] || $_REQUEST['class'] || isset($_REQUEST['gender']) && isset($_REQUEST['Courses'])){
     echo"<h1>Your Given Values Are:</h1>";
     echo "Name:".$_REQUEST['name']."<br>";
     echo "E-mail:".$_REQUEST['e-mail']."<br>";
     echo "Group#:".$_REQUEST['group']."<br>";
     echo "Class-details:".$_REQUEST['class']."<br>";
     echo "Gender:".$_REQUEST['gender']."<br>";
+    echo "your courses are: ";
+for($i=0; $i<count($_REQUEST['Courses']); $i++){
+         echo " ".$_REQUEST['Courses'][$i];
    };
-   $Courses = $_REQUEST['Courses'];
-echo "your courses are: ";
-for($i=0; $i<count($Courses); $i++){
-    if($Courses){
-         echo " ".$Courses[$i];
-    };
-    
    };
+
+
 };
 ?>
 
